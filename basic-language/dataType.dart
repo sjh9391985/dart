a5012a7c3ea61c4e1f8f84d21f0579860e3089d1
sjh9391985 <sjh9391985@gmail.com>
2023-06-12 22:19:01 +0900

    feat: 'List 사용법'

---
+++ basic-language/dataType.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+
+  var giveMeFive = true;
+
+  // 1. List
+  var numbers = [
+    1,2,3,4,
+     if(giveMeFive) 5, // collection if
+    ]; // or List<int> numbers ~
+
+  numbers.add(5);
+  print(numbers);
+
+
+}
