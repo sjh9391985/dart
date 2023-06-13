5e26ace5bdd8692ea8e5134e876c9f3bbf892e3d
sjh9391985 <sjh9391985@gmail.com>
2023-06-13 20:03:58 +0900

    feat: 'function 사용법'

---
+++ basic-language/function.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  sayHello("toya");
+}
+
+void sayHello(String name){
+  print("hello! $name nice to meet you");
+}
+// 1줄일 경우 사용 : String sayHello(String name) => "hello! $name nice to meet you";
