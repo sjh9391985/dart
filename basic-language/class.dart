447befd73f5c64d65bc43c560150794023bce588
sjh9391985 <sjh9391985@gmail.com>
2023-06-14 21:56:43 +0900

    feat: '보다 나은 생성자 사용법'

--- basic-language/class.dart
+++ basic-language/class.dart
@@ -1,8 +1,15 @@
 class Player {
-  late String name; // final 붙이면 변경 불가능
-  late int xp;
+  String name; // final 붙이면 변경 불가능
+  int xp;
+  String team;
+  int age;
 
-  Player(this.name, this.xp);
+  Player({
+    required this.name,
+    required this.xp,
+    required this.age,
+    required this.team
+  });
 
   void sayHello() {
     print("my name is $name");
@@ -10,7 +17,12 @@ class Player {
 }
 
 void main(List<String> args) {
-  var player = Player("toya", 1500);
+  var player = Player(
+    team: "tosu",
+    age: 20,
+    name: "toya",
+    xp: 1500,
+  );
   // var player = Player();
   print(player.name);
   player.name = 'suya';
